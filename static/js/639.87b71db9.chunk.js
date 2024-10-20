12c7abea4ec2c578de3b6931d2323306902d4528
김수진 <nuu042@naver.com>
2024-10-20 20:00:01 +0900

    Updates

---
+++ static/js/639.87b71db9.chunk.js
@@ -0,0 +1,110 @@
+"use strict";(self.webpackChunksujin_portf=self.webpackChunksujin_portf||[]).push([[639],{639:(e,t,i)=>{i.r(t),i.d(t,{default:()=>A});var r=i(579);var o=i(464);const n=e=>{const{data:t}=e;return(0,r.jsx)(l,{children:(0,r.jsx)(d,{children:(0,r.jsx)(s,{children:null===t||void 0===t?void 0:t.title})})},null===t||void 0===t?void 0:t.key)},l=o.Ay.div`
+    width: calc(28rem - 12px);
+    border-radius: 20px;
+    height: 397px;
+    border-radius: 16px;
+    background-color: #d9f0f9;
+    display: inline-block;
+    margin: 0 10px 0 10px;    
+    position: relative;
+    cursor: pointer;
+`,d=o.Ay.span` 
+    padding: 0 0 12px 12px;
+    border-radius: 0 0 0 20px;
+    background-color: white;
+    text-align: center;
+    position: absolute;
+    line-height: 1.5;
+    right: -2px;
+    top: -2px;
+    width: 168px;
+    ::before{
+        content: "";
+        display: block;
+        position: absolute;
+        width: 18px;
+        height: 18px;
+        background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 18 18'%3E%3Cpath d='M0 2c8.8 0 16 7.2 16 16h2V0H0v2z' fill='%23fff' fill-rule='evenodd' clip-rule='evenodd'/%3E%3C/svg%3E");
+        background-size: 100% auto;
+        background-position: 50%;
+        background-repeat: no-repeat;
+        left: -16px;        
+        top: 0;
+    }
+    ::after{
+        content: "";
+        display: block;
+        position: absolute;
+        width: 18px;
+        height: 18px;
+        background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 18 18'%3E%3Cpath d='M0 2c8.8 0 16 7.2 16 16h2V0H0v2z' fill='%23fff' fill-rule='evenodd' clip-rule='evenodd'/%3E%3C/svg%3E");
+        background-size: 100% auto;
+        background-position: 50%;
+        background-repeat: no-repeat;
+        bottom: -16px;
+        right: 0;
+    } 
+`,s=o.Ay.span`
+    width: 140px;
+    padding: 0 1.2rem;
+    height: 40px;
+    line-height: 40px;
+    font-size: 14px;
+    border-radius: 20px;
+    background-color : #d9f0f9;
+    margin: 2px 2px 0 0;
+    color: #000;
+    text-align: center;
+    display: inline-block;
+    margin-right: 5px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+`,a=()=>(0,r.jsx)(r.Fragment,{children:(0,r.jsxs)(c,{children:[(0,r.jsxs)(p,{children:[(0,r.jsx)("h2",{children:"\ubc1c\uc804\ud558\ub294 \uac1c\ubc1c\uc790"}),(0,r.jsx)("p",{children:"\uae40\uc218\uc9c4"})]}),(0,r.jsx)(h,{children:(0,r.jsx)(u,{})})]})}),c=o.Ay.div`
+    padding: 3rem 2rem;
+    background-color: #ffffff;
+    font-weight: 600;
+    font-size: 2rem;
+    text-align: center;
+    width: 100%;
+`,p=o.Ay.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    font-size: 7rem;
+    font-family: 'Binggrae-Two';
+    color: black;
+    margin-bottom: 3rem;
+`,h=o.Ay.div`
+    display: flex;
+    justify-content: center;
+`,u=o.Ay.div`
+    width: 90%;
+    background-color: black;
+    height: 50rem;
+`;var x=i(43);const g=[{key:0,title:"test11111111111111111111111111111111111111111111111"},{key:1,title:"test22222"},{key:2,title:"test3333"},{key:3,title:"test1111"},{key:4,title:"test22222"},{key:5,title:"test3333"}],f=e=>{const{data:t}=e,i=(0,x.useRef)(null),[o,l]=(0,x.useState)(!1),[d,s]=(0,x.useState)(0),a=()=>{l(!1)},c=((e,t)=>{let i=!1;return function(){for(var r=arguments.length,o=new Array(r),n=0;n<r;n++)o[n]=arguments[n];i||(i=!0,setTimeout((()=>{e(...o),i=!1}),t))}})((e=>{o&&(i.current.scrollLeft=d-e.pageX)}),20);return(0,r.jsxs)(v,{children:[(0,r.jsx)(m,{children:(0,r.jsx)("h2",{children:null===t||void 0===t?void 0:t.title})},null===t||void 0===t?void 0:t.key),0===(null===t||void 0===t?void 0:t.key)?(0,r.jsx)(y,{onMouseDown:e=>{e.preventDefault(),l(!0),s(e.pageX+i.current.scrollLeft)},onMouseMove:c,onMouseUp:a,onMouseLeave:a,ref:i,children:g.map((e=>(0,r.jsx)(n,{data:e})))}):(0,r.jsx)(r.Fragment,{})]},null===t||void 0===t?void 0:t.key)},v=o.Ay.div`
+    width: 100%;
+`,m=o.Ay.div`
+    margin: 2rem 5rem;
+`,y=o.Ay.div`
+    width: 100%;
+    overflow-x: scroll;
+    scrollbar-width: none;
+    scroll-behavior: smooth;
+    white-space: nowrap;
+    padding: 0.5rem 4rem;
+`,k=()=>(0,r.jsx)(w,{children:(0,r.jsxs)(b,{children:[(0,r.jsx)("p",{style:{marginBottom:10},children:"\ubcf8 \ud398\uc774\uc9c0\ub294 \uc0c1\uc5c5\uc801 \ubaa9\uc801\uc774 \uc544\ub2cc \uac1c\uc778 \ud3ec\ud2b8\ud3f4\ub9ac\uc624\uc6a9\uc73c\ub85c \uc81c\uc791\ub418\uc5c8\uc2b5\ub2c8\ub2e4."}),(0,r.jsx)("p",{children:"\xa9 2024 Kim Sujin. All Rights Reserved."})]})}),w=o.Ay.div`
+    width: 100%;
+    height: 7rem;
+    display: flex;
+    justify-content: center;
+    background-color: #e9e9e9 ;
+    padding: 20px 5rem;
+`,b=o.Ay.div`
+    padding-top: 1rem;
+    text-align: center;
+`,j=[{key:0,title:"PROJECT",route:""},{key:1,title:"CAREER",route:""},{key:2,title:"CONTACT",route:""}],A=()=>(0,r.jsxs)(C,{children:[(0,r.jsx)(a,{}),j.map((e=>(0,r.jsx)(f,{data:e}))),(0,r.jsx)(k,{})]}),C=o.Ay.div`
+    width: 100%;
+    height: 100%;
+`}}]);
+//# sourceMappingURL=639.87b71db9.chunk.js.map
